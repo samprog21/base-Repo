2c8908cdc4a617a51387bd9aa79398afcddab007
sam <samprog21@gmail.com>
2022-12-11 19:04:59 +0530

    test

---
+++ source/src/pages/SignUpPage.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const SignUpPage = () => {
+   return <h1>Sign Up</h1>;
+};
+
+export default SignUpPage;
---
+++ source/src/pages/SignUpPage.spec.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import SignUpPage from './SignUpPage';
+import { render, screen } from '@testing-library/react';
+
+it('has header', () => {
+   render(<SignUpPage />);
+   const header = screen.queryByRole('heading', {
+      name: 'Sign Up',
+   });
+   expect(header).toBeInTheDocument();
+});
